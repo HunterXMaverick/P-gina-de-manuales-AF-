8e45c11fa33d488d4d172a2e16aa9d9be27d6153
HunterXMaveric <jfo.silva@yavirac.edu.ec>
2018-09-07 16:19:50 -0500

    primera fase completara

--- Formulario/src/app/app.component.ts
+++ Formulario/src/app/app.component.ts
@@ -7,6 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title: string = 'Formulario';
+  msg: string = '';
+  hideUpdate: boolean = true;
 
   documento = [
     /*{'name': 'JS', position: 'manager'},
@@ -17,20 +19,37 @@ export class AppComponent {
   model: any = {};
   model2: any = {};
 
-  agregarDocumento(): void{
+  agregarDocumento(): void {
     this.documento.push(this.model);
+    this.msg = 'Campo Agregado';
   }
 
-  borrarDocumento(): void{
-
+  borrarDocumento(i): void {
+    var respuesta = confirm('Seguro que quiere borrar');
+    if (respuesta) {
+      this.documento.splice(i, 1);
+      this.msg = 'Campo Eliminado';
+    }
   }
   myValue;
-  editarDocumento(i): void{
+  editarDocumento(i): void {
+    this.hideUpdate = false; 
     this.model2.name = this.documento[i].name;
     this.myValue = i;
   }
 
-  actualizarDocumento(): void{
-    console.log(this.model2)
+  actualizarDocumento(): void {
+    let i = this.myValue;
+    for (let j = 0; j < this.documento.length; j++) {
+      if (i == j) {
+        this.documento[i] = this.model2;
+        this.msg = 'Campo Actualizado';
+        this.model2 = {};
+      }
+    }
+  }
+
+  closeAlert(): void {
+    this.msg = '';
   }
 }
